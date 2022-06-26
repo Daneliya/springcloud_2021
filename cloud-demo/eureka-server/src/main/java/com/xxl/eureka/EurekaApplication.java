c0cbb61ad8c09ba9e7a74670206fbfa31bcb3f7a
XXL <luckily126163@126.com>
2022-06-26 23:48:45 +0800

    eureka微服务搭建

---
+++ cloud-demo/eureka-server/src/main/java/com/xxl/eureka/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.xxl.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author xxl
+ * @date 2022/6/26 12:57
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
